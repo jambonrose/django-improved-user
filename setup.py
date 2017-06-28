b0b0727e86175c6f7bbbde8b6f0a60e70f5ca38c
Andrew Pinkham <code@andrewsforge.com>
2017-06-28 16:34:21 -0400

    Update setup.py with new repo and details

--- setup.py
+++ setup.py
@@ -9,26 +9,30 @@ with io.open('README.rst', encoding='utf8') as readme:
 
 
 setup(
-    name='django-simpleuser',
+    name='django-improved-user',
     version='0.1.0',
     description='A custom Django user model for best practices email-based login.',
     long_description=long_description,
-    author='Russell Keith-Magee',
-    author_email='russell@keith-magee.com',
-    url='http://github.com/jambonsw/simpleuser/',
+    author=(
+        'Russell Keith-Magee <russell at keith-magee dot com>, '
+        'Andrew Pinkham <hello at andrewsforge dot com>',
+    ),
+    author_email='hello at andrewsforge dot com',
+    url='https://github.com/jambonsw/django-improved-user/',
     packages=find_packages(exclude=['docs', 'tests']),
     install_requires=[
         'django>=1.8'
     ],
-    license='New BSD',
+    license='Simplified BSD License',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3 :: Only',
         'Topic :: Software Development',
         'Topic :: Utilities',
