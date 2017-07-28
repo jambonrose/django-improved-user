d62ad9455651a8aac2cb840768e18213ef131269
Andrew Pinkham <code@andrewsforge.com>
2017-07-28 11:22:56 -0400

    Sort imports in models.py

--- src/improved_user/models.py
+++ src/improved_user/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import (
-    AbstractBaseUser, BaseUserManager, PermissionsMixin,)
+    AbstractBaseUser, BaseUserManager, PermissionsMixin,
+)
 from django.core.mail import send_mail
 from django.db import models
 from django.utils import timezone
