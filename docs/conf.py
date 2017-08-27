c1aacd2bc477e0a7d9a5e073ea8e8d39f5af669c
Andrew Pinkham <code@andrewsforge.com>
2017-08-27 09:58:43 -0400

    Fix Sphinx build in Travis - disable unused static

--- docs/conf.py
+++ docs/conf.py
@@ -203,7 +203,7 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the built-in static files,
 # so a file named "default.css" will overwrite the built-in "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
