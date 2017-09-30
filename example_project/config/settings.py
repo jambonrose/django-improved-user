746140a65c223032cdb0c5c79a0f6f6dea89747a
Andrew Pinkham <code@andrewsforge.com>
2017-09-30 17:29:14 -0400

    Enable coverage on integration project with Tox

--- example_project/config/settings.py
+++ example_project/config/settings.py
@@ -80,6 +80,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'debug': True,
         },
     },
 ]
