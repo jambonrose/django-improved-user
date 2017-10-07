0281d7ffac453cc40043ba6f224a493c39d34e35
Andrew Pinkham <code@andrewsforge.com>
2017-10-07 16:54:55 -0400

    Create new User model in extension project

--- example_extension_project/config/settings.py
+++ example_extension_project/config/settings.py
@@ -94,6 +94,8 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/stable/ref/settings/#auth-password-validators
 
+AUTH_USER_MODEL = 'user_extension.User'
+
 AUTH_PREFIX = 'django.contrib.auth.password_validation.'
 AUTH_PASSWORD_VALIDATORS = [
     {
