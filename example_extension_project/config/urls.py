d21d0f36d46c0b86dca5780642bcef9902ed9e43
Andrew Pinkham <code@andrewsforge.com>
2017-10-07 16:07:03 -0400

    Generate example extension project for testing

---
+++ example_extension_project/config/urls.py
@@ -0,0 +1,12 @@
+"""Integration project URL Configuration"""
+from django.contrib import admin
+
+try:
+    from django.urls import re_path
+except ImportError:
+    from django.conf.urls import url as re_path
+
+
+urlpatterns = [
+    re_path(r'^admin/', admin.site.urls),
+]
