f945c4a1e282237e93959c832c04c5c642de1a3a
Andrew Pinkham <code@andrewsforge.com>
2017-10-15 13:20:33 -0400

    Generate user_extension app

--- example_extension_project/config/settings.py
+++ example_extension_project/config/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
+    'user_extension.apps.UserExtensionConfig',
 ]
 
 if DjangoVersion >= (1, 10):
---
+++ example_extension_project/user_extension/apps.py
@@ -0,0 +1,7 @@
+"""Application Definition File"""
+from django.apps import AppConfig
+
+
+class UserExtensionConfig(AppConfig):
+    """AppConfig definition for user extension code"""
+    name = 'user_extension'
