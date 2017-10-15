35a3836ef3827fb386e14365bc5f0dd1848eb645
Andrew Pinkham <code@andrewsforge.com>
2017-10-15 10:19:49 -0400

    📚 Change beta classifier to production/stable

--- setup.py
+++ setup.py
@@ -171,7 +171,7 @@ setup(
     ),
     license='BSD License',
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
