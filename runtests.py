417e704b2921bbed72e5b694b8692254cfe8944e
Andrew Pinkham <code@andrewsforge.com>
2021-11-05 21:46:19 -0400

    🐛 Fix admin.W411: add request ctx to templates

--- runtests.py
+++ runtests.py
@@ -65,6 +65,7 @@ def configure_django():
                     "context_processors": [
                         "django.contrib.auth.context_processors.auth",
                         "django.contrib.messages.context_processors.messages",
+                        "django.template.context_processors.request",
                     ],
                 },
             }
