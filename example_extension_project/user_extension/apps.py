2cf305045c9a772bada36cdfc3869c1b35bca7cb
Andrew Pinkham <code@andrewsforge.com>
2021-11-05 21:49:54 -0400

    🐛 Fix models.W042: define default_auto_field

--- example_extension_project/user_extension/apps.py
+++ example_extension_project/user_extension/apps.py
@@ -4,4 +4,6 @@ from django.apps import AppConfig
 
 class UserExtensionConfig(AppConfig):
     """AppConfig definition for user extension code"""
-    name = 'user_extension'
+
+    name = "user_extension"
+    default_auto_field = "django.db.models.AutoField"
