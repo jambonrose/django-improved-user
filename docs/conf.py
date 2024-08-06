9ff56a7347ebfd9f448792fdba5f20f16b0946e5
Andrew Pinkham <code@andrewsforge.com>
2024-08-06 11:13:32 -0400

    🐛 Fix documentation build
    PR #298

--- docs/conf.py
+++ docs/conf.py
@@ -130,10 +130,6 @@ def setup(app):
 
 # -- General configuration ------------------------------------------------
 
-# If your documentation needs a minimal Sphinx version, state it here.
-#
-# needs_sphinx = '1.0'
-
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
@@ -181,7 +177,7 @@ release = "2.0.0"
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
